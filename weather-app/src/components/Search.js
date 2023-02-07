596bec3f78a669c4ec69ee9bcf6dc41463ff1d12
Sedakat <sedakatekmekci16@gmail.com>
2023-02-07 15:36:19 +0300

    Update Search.js

--- weather-app/src/components/Search.js
+++ weather-app/src/components/Search.js
@@ -20,15 +20,13 @@ function Search() {
     };
 
     return (
+        <input
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder='Enter Location'
+            type="text" />
 
-        <div className="search">
-            <input
-                value={location}
-                onChange={event => setLocation(event.target.value)}
-                onKeyPress={searchLocation}
-                placeholder='Enter Location'
-                type="text" />
-        </div>
 
     )
 }
