1aea1eb76f8bfd36c32e9aa85c2274623e7e8094
Sedakat <sedakatekmekci16@gmail.com>
2023-02-07 15:43:51 +0300

    Update Body.js

---
+++ weather-app/src/components/Body.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useData } from '../Context/WeatherContext';
+
+function Body() {
+    const { data, setData } = useData();
+    return (
+        <div className="top">
+            <div className="location">
+                <p>{data.name}</p> {/*display selected city name */}
+            </div>
+            <div className="temp">
+                {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null} {/*display temperature */}
+            </div>
+            <div className="description">
+                {data.weather ? <p>{data.weather[0].main}</p> : null} {/*display description */}
+            </div>
+        </div>
+    )
+}
+
+export default Body
