b69c052cf293a7357f83c9192c7335b126fcebd9
Sedakat <sedakatekmekci16@gmail.com>
2023-02-07 15:51:21 +0300

    Update Body.js

--- weather-app/src/components/Body.js
+++ weather-app/src/components/Body.js
@@ -4,23 +4,37 @@ import { useData } from '../Context/WeatherContext';
 function Body() {
     const { data, setData } = useData();
     return (
-        <div className="container">
+        <div className='container'>
             <div className="top">
                 <div className="location">
-                    <p>{data.name}</p> {/*display selected city name */}
+                    <p>{data.name}</p>
                 </div>
                 <div className="temp">
-                    {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null} {/*display temperature */}
+
+                    {data.main ? <h1>{data.main.temp.toFixed()}°C </h1> : null}
                 </div>
                 <div className="description">
-                    {data.weather ? <p>{data.weather[0].main}</p> : null} {/*display description */}
+                    {data.weather ? <p>{data.weather[0].main}</p> : null}
                 </div>
             </div>
-            <div>
-                <h1>Something</h1>
-            </div>
-
+            {data.name != undefined &&
+                <div className="bottom">
+                    <div className="feels">
+                        {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C </p> : null}
+                        <p >Feels Like</p>
+                    </div>
+                    <div className="humidity">
+                        {data.main ? <p className='bold'>{data.main.humidity} </p> : null}
+                        <p >Humidity</p>
+                    </div>
+                    <div className="wind">
+                        {data.main ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+                        <p>Wind Speed</p>
+                    </div>
+                </div>
+            }
         </div>
+
 
     )
 }
