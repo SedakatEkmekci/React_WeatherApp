65c9fb98101a6a63800400db8c1bec9c8b0063ca
Sedakat <sedakatekmekci16@gmail.com>
2023-02-07 15:21:35 +0300

    Update WeatherContext.js

--- weather-app/src/context/WeatherContext.js
+++ weather-app/src/context/WeatherContext.js
@@ -1,3 +1,12 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const WeatherContext = createContext();
+
+export const WeatherProvider = ({ children }) => {
+    const [data, setData] = useState({});
+
+    return <WeatherContext.Provider value={values}>{children}</WeatherContext.Provider>
+
+};
+
+export const useData = () => useContext(WeatherContext);
