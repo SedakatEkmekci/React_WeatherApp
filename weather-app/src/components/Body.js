cd69469be41344a6207dab60e54456b3a38d45fa
Sedakat <sedakatekmekci16@gmail.com>
2023-02-07 15:46:10 +0300

    Update Body.js

--- weather-app/src/components/Body.js
+++ weather-app/src/components/Body.js
@@ -4,17 +4,24 @@ import { useData } from '../Context/WeatherContext';
 function Body() {
     const { data, setData } = useData();
     return (
-        <div className="top">
-            <div className="location">
-                <p>{data.name}</p> {/*display selected city name */}
+        <div className="container">
+            <div className="top">
+                <div className="location">
+                    <p>{data.name}</p> {/*display selected city name */}
+                </div>
+                <div className="temp">
+                    {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null} {/*display temperature */}
+                </div>
+                <div className="description">
+                    {data.weather ? <p>{data.weather[0].main}</p> : null} {/*display description */}
+                </div>
             </div>
-            <div className="temp">
-                {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null} {/*display temperature */}
+            <div>
+                <h1>Something</h1>
             </div>
-            <div className="description">
-                {data.weather ? <p>{data.weather[0].main}</p> : null} {/*display description */}
-            </div>
+
         </div>
+
     )
 }
 
