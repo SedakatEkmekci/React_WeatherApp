ea3fbf29586f79ed0675beafb409442225d32652
Sedakat <sedakatekmekci16@gmail.com>
2023-02-07 14:13:41 +0300

    Update Search.js

--- weather-app/src/components/Search.js
+++ weather-app/src/components/Search.js
@@ -1,33 +1,33 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-function Search() {
-    const [data, setData] = useState({});
-    const [location, setLocation] = useState('');
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=istanbul&units=metric&appid=dba667ca31672d6b523c15ae1accbdec`;
+// function Search() {
+//     const [data, setData] = useState({});
+//     const [location, setLocation] = useState('');
+//     const url = `https://api.openweathermap.org/data/2.5/weather?q=istanbul&units=metric&appid=dba667ca31672d6b523c15ae1accbdec`;
 
-    const searchLocation = (event) => {
-        if (event.key === 'Enter') {
-            axios.get(url).then((response) => {
-                setData(response.data)
-                console.log(response.data)
-            })
-            setLocation('');
-        }
-    };
-    return (
-        <div className="app">
-            <div className="search">
-                <input
-                    value={location}
-                    onChange={event => setLocation(event.target.value)}
-                    onKeyPress={searchLocation}
-                    placeholder='Enter Location'
+//     const searchLocation = (event) => {
+//         if (event.key === 'Enter') {
+//             axios.get(url).then((response) => {
+//                 setData(response.data)
+//                 console.log(response.data)
+//             })
+//             setLocation('');
+//         }
+//     };
+//     return (
+//         <div className="app">
+//             <div className="search">
+//                 <input
+//                     value={location}
+//                     onChange={event => setLocation(event.target.value)}
+//                     onKeyPress={searchLocation}
+//                     placeholder='Enter Location'
 
-                    type="text" />
-            </div>
-        </div>
-    )
-}
+//                     type="text" />
+//             </div>
+//         </div>
+//     )
+// }
 
-export default Search
+// export default Search
