f4bd343313dd87392269c38020b02fd64ae71d7b
Sedakat <sedakatekmekci16@gmail.com>
2023-02-07 16:26:16 +0300

    Update Footer.js

--- weather-app/src/components/Footer.js
+++ weather-app/src/components/Footer.js
@@ -3,7 +3,11 @@ import React from 'react'
 function Footer() {
     return (
         <div>
-
+            <footer className="info">
+                <p>Click to edit a todo</p>
+                <p>Created by <a href="https://github.com/SedakatEkmekci">Sedakat Ekmekçi</a></p>
+                <p>Reach data from <a href="https://openweathermap.org">Open Weather</a></p>
+            </footer>
         </div>
     )
 }
